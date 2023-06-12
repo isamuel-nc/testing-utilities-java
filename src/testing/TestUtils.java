851d950776c51a713f888aa01a9df3cf8e87373d
isamuel-nc <83732023+isamuel-nc@users.noreply.github.com>
2023-06-12 11:48:06 +0100

    Update TestUtils.java

--- src/testing/TestUtils.java
+++ src/testing/TestUtils.java
@@ -195,7 +195,7 @@ public class TestUtils extends Mod{
     }
 
     public static boolean disableCampaign(){
-        return state.isCampaign() && !(OS.username.equals("MEEP") && settings.getBool("tu-meep-privileges"));
+        return false;
     }
 
     public static boolean click(){
